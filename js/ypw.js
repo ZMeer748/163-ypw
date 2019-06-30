6c26e8d7163481f0fe912267b82d09b15f8495bd
SissonMore <sissonmore@163.com>
2019-06-30 20:48:08 +0800

    add user.html

---
+++ js/ypw.js
@@ -0,0 +1,20 @@
+$(document).on('click', '#main-footer-btn-discover', function () {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "1000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    toastr.info("暂不支持");
+});
